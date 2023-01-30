701f70fb0d52108d0aba263272c9c0288631a058
Edozie <phynnedo@gmail.com>
2023-01-30 16:46:26 +0100

    fix: added the UI components for the Navbar

--- src/components/UI/navbar.tsx
+++ src/components/UI/navbar.tsx
@@ -1,8 +1,59 @@
+import React from "react";
+import uuid from "react-uuid";
+
+
+
+import styles from  "./styles/navbar.module.scss";
+import switchOrg from "../assets/icons/switchOrg.png";
+import dashboard from "../assets/icons/dashboard.png";
+import {data} from "../data/navbarData";
+
 const Navbar = () => {
+
     return (
-        <nav>
-            
-        </nav>
+      <nav className={styles.navbar}>
+        <div className={`${styles["navbar-org"]}`}>
+          <img src={switchOrg} alt="Switch Organization Icon" />
+          <p>Switch Organization</p>
+        </div>
+        <div className={`${styles["navbar-dashboard"]}`}>
+          <img src={dashboard} alt="Dashboard Icon" />
+          <p>Dashboard</p>
+        </div>
+        <div className={`${styles["navbar-customers"]}`}>
+          <p>CUSTOMERS</p>
+          <ul>
+            {data[0].map((customer) => (
+              <li key={uuid()} className={styles.tabs}>
+                <img src={customer.icon} alt="Customers Icon" />
+                <p>{customer.text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`${styles["navbar-businesses"]}`}>
+          <p>BUSINESSES</p>
+          <ul>
+            {data[1].map((business) => (
+              <li key={uuid()} className={styles.tabs}>
+                <img src={business.icon} alt="Businesses Icon" />
+                <p>{business.text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`${styles["navbar-settings"]}`}>
+          <p>SETTINGS</p>
+          <ul>
+            {data[2].map((setting) => (
+              <li key={uuid()} className={styles.tabs}>
+                <img src={setting.icon} alt="Settings Icon" />
+                <p>{setting.text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
     );
 }
  
---
+++ src/components/data/navbarData.js
@@ -0,0 +1,49 @@
+import users from "../assets/icons/users.png";
+import guarantors from "../assets/icons/guarantors.png";
+import loans from "../assets/icons/loans.png";
+import decisionModels from "../assets/icons/decisionModels.png";
+import savings from "../assets/icons/savings.png";
+import loanRequests from "../assets/icons/loanRequests.png";
+import whiteList from "../assets/icons/whiteList.png";
+import karma from "../assets/icons/karma.png";
+import organization from "../assets/icons/organization.png";
+import savingsProducts from "../assets/icons/savingsProducts.png";
+import feesAndCharges from "../assets/icons/feesAndCharges.png";
+import transactions from "../assets/icons/transactions.png";
+import services from "../assets/icons/services.png";
+import serviceAccount from '../assets/icons/serviceAccount.png';
+import settlements from "../assets/icons/services.png";
+import reports from "../assets/icons/reports.png";
+import preferences from "../assets/icons/preferences.png";
+import feesAndPricing from "../assets/icons/feesAndPricing.png";
+import auditLogs from "../assets/icons/auditLogs.png";
+
+
+export const data = [
+    [
+        { icon: users, text: "Users" },
+        { icon: guarantors, text: "Guarantors" },
+        { icon: loans, text: "Loans" },
+        { icon: decisionModels, text: "Decision Models" },
+        { icon: savings, text: "Savings" },
+        { icon: loanRequests, text: "Loan Requests" },
+        { icon: whiteList, text: "Whitelist" },
+        { icon: karma, text: "Karma" },
+    ],
+    [
+        { icon: organization, text: "Organization" },
+        { icon: loanRequests, text: "Loan Products" },
+        { icon: savingsProducts, text: "Saving Products" },
+        { icon: feesAndCharges, text: "Fees and Charges" },
+        { icon: transactions, text: "Transactions" },
+        { icon: services, text: "Services" },
+        { icon: serviceAccount, text: "Service Account" },
+        { icon: settlements, text: "Settlements" },
+        { icon: reports, text: "Reports" },
+    ],
+    [
+        { icon: preferences, text: "Preferences" },
+        { icon: feesAndPricing, text: "Fees and Pricing" },
+        { icon: auditLogs, text: "Audit Logs" },
+    ]
+];
